68439e43931d4c6281a6658541b5b321e3ecb88c
andyjohn23 <andrewnick906@gmail.com>
2022-08-04 23:02:37 +0300

    created the navbar and styled it

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Topbar.css"
 import { Search } from "@mui/icons-material";
 // import { Avatar } from "@mui/material";
-import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 function Topbar() {
     return (
@@ -10,27 +10,16 @@ function Topbar() {
             <Navbar expand="lg" className="navbar" fixed="top">
                 <Container fluid>
                     <Navbar.Brand>
-                        <h1>Expressify</h1>
+                        <Nav.Link to="/"><h1>Expressify</h1></Nav.Link>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <div className="category">
-                                <NavDropdown title="Categories" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">
-                                        Another action
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">
-                                        Something
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            </div>
+                        <Nav className="ml-auto">
                             <div className="searchbar">
                                 <Search className="searchIcon" />
                                 <input
                                     type="search"
-                                    placeholder="Search by title or author"
+                                    placeholder="Search"
                                     className="searchInput"
                                 />
                             </div>
