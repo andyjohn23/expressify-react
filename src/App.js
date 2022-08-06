dac6e258c8359d3d49b0f49d860427dd2848c2c9
andyjohn23 <andrewnick906@gmail.com>
2022-08-06 12:31:40 +0300

    styling the homeslider component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import HomePage from './container/HomePage';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Homeslider/HomeSlider.js
+++ src/components/Homeslider/HomeSlider.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import "./HomeSlider.css"
+import { Link } from "react-router-dom"
+import SliderImage from "../../assets/images/blog.png"
 
 function HomeSlider() {
     return (
         <div className='homeSlider__container'>
             <div className='homeSlider__left'>
                 <h1>Express Yourself.</h1>
+                <p>Discover stories, thinking, and expertise from writers on any topic.</p>
+                <Link to="" style={{ textDecoration: "none" }}>
+                    <button>Start reading</button>
+                </Link>
             </div>
             <div className='homeSlider__right'>
-
+                <img src={SliderImage} alt="blogImage" />
             </div>
-
-
         </div>
     )
 }
