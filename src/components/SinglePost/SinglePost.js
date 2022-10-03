286981baca3c608393528408a69f8e273d81865f
andyjohn23 <andrewnick906@gmail.com>
2022-10-03 20:26:18 +0300

    created the ability for only blog owner to delete blog

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./SinglePost.css"
 import singleImage from "../../assets/images/blog.png"
 import { Avatar } from "@mui/material"
@@ -6,12 +6,13 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useLocation } from "react-router";
 import axios from 'axios'
+import { Context } from '../../context/Context';
 
 function SinglePost() {
     const location = useLocation()
     const path = location.pathname.split("/")[2];
     const [post, setPost] = useState({});
-    const [user, setUser] = useState({});
+    const { user } = useContext(Context)
 
     useEffect(() => {
         const getPost = async () => {
@@ -20,14 +21,6 @@ function SinglePost() {
         }
         getPost()
     }, [path])
-
-    useEffect(() => {
-        const getUser = async () => {
-            const response = await axios.get("http://localhost:3000/api/users")
-            setUser(response.data)
-        }
-        getUser()
-    }, [])
 
     return (
         <div className='singlePost__container'>
@@ -40,10 +33,12 @@ function SinglePost() {
                     <div className='title'>
                         <h1>{post.title}</h1>
                     </div>
-                    <div className='Icons'>
-                        <EditIcon className='editIcon' style={{ cursor: "pointer" }} />
-                        <DeleteIcon className='deleteIcon' style={{ cursor: "pointer" }} />
-                    </div>
+                    {post.user?.id === user?.id && (
+                        <div className='Icons'>
+                            <EditIcon className='editIcon' style={{ cursor: "pointer" }} />
+                            <DeleteIcon className='deleteIcon' style={{ cursor: "pointer" }} />
+                        </div>
+                    )}
                 </div>
                 <img src={singleImage} alt="postimage" className='postImage' />
                 <p>{post.content}</p>
