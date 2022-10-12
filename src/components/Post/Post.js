04d7e4f831f6b8ccd3a3dc6e2c67cbe5bc2b5234
andyjohn23 <andrewnick906@gmail.com>
2022-10-12 11:11:32 +0300

    styling the post card

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -10,29 +10,23 @@ import Card from 'react-bootstrap/Card';
 function Post({ post }) {
     return (
         <div className='post__container'>
-            <Link to={`/post/${post.id}`} style={{ textDecoration: "none" }}>
-                <Card className='post__card'>
-                    <Card.Body>
-                        <Card.Title>
-                            <div className='post__owner'>
-                                <div className="post__avatar">
-                                    <Avatar src={postImage} className='user__avatar' />
-                                    <span>{post.user?.username}</span>
-                                </div>
-                                <div className="post__time">
-                                    {post.created_at}
-                                </div>
-                            </div>
-                        </Card.Title>
-                    </Card.Body>
-                    <Card.Img src={postImage} />
+            <Card className='post__card'>
+                <Link to="" style={{ textDecoration: "none" }}>
+                    <Card.Title>
+                        <div className='post__owner'>
+                            <Avatar src={postImage} className='user__avatar' />
+                            <span>{post.user?.username}</span>
+                        </div>
+                    </Card.Title>
+                </Link>
+                <Link to={`/post/${post.id}`} style={{ textDecoration: "none" }}>
+                    <Card.Img src={postImage} className="post__image" />
                     <Card.Body>
                         <Card.Title>{post.title}</Card.Title>
                         <Card.Text>{post.content}</Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
                     </Card.Body>
-                </Card>
-            </Link>
+                </Link>
+            </Card>
         </div>
     )
 }
